cc9f3ae4f7d789a0f860078d991f494b0a3249cf
leon-kfd <kfd_personal@163.com>
2021-04-02 18:07:20 +0800

    feat:build option

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dashboard/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
