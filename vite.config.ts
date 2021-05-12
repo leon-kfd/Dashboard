7cab198c1d4130ae0185efbb544ed2ec9acfb7fd
Leon-kfd <kfd_personal@163.com>
2021-05-12 19:46:34 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/Dashboard/',
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
