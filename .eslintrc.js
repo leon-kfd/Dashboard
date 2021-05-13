1c2caee305512f6500b13ae5a96ce8b163021bf9
leon-kfd <kfd_personal@163.com>
2021-05-13 13:59:19 +0800

    feat: add auto build crx file CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,7 @@ module.exports = {
     ],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Dashboard/',
+  base: process.env.BUILD_MODE === 'crx' ? '/' : '/Dashboard/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
@@ -29,7 +29,8 @@ export default defineConfig({
           'element-plus': ['element-plus']
         }
       }
-    }
+    },
+    outDir: process.env.BUILD_MODE === 'crx' ? 'crx' : 'dist'
   },
   server: {
     proxy: {
