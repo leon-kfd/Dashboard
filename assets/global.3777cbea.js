23cc9c728cd4815fdad9c369c9e99f83026fe500
leon-kfd <kfd_personal@163.com>
2021-05-17 07:43:59 +0000

    Deploying to gh-pages from  @ c4647e302ee8c618098d3c54b1a833d6bfc034a4 🚀

--- assets/global.3777cbea.js
+++
@@ -1,1 +0,0 @@
-const n="https://kongfandong.cn";export{n as a};
---
+++ sw.js
@@ -0,0 +1,33 @@
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js')
+
+workbox.setConfig({
+  debug: false
+});
+
+console.log('hello sw.')
+
+// Cache image.
+workbox.routing.registerRoute(
+  ({ request }) => {
+    return request.destination === 'image' ||
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'font' ||
+    request.destination === 'video'
+  },
+  new workbox.strategies.CacheFirst({
+    cacheName: 'sources',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200],
+      }),
+      // Don't cache more than 50 items, and expire them after 7 days
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 60 * 60 * 24 * 7, // 7 Days
+      }),
+    ]
+  })
+);
