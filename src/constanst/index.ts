1b284d133e5f768972816d339c8e63ef65956892
leon-kfd <kfd_personal@163.com>
2021-05-28 16:03:53 +0800

    feat: change material img url

--- src/constanst/index.ts
+++ src/constanst/index.ts
@@ -2,62 +2,62 @@ export const MATERIAL_LIST_MAP = {
   1: {
     label: 'Empty',
     text: '占位区块',
-    img: 'http://cdn.kongfandong.cn/img/Empty.png'
+    img: '//kongfandong.cn/images/capture/Empty.png'
   },
   2: {
     label: 'Clock',
     text: '时钟',
-    img: 'http://cdn.kongfandong.cn/img/Clock.png'
+    img: '//kongfandong.cn/images/capture/Clock.png'
   },
   3: {
     label: 'Verse',
     text: '随机古诗',
-    img: 'http://cdn.kongfandong.cn/img/Verse.png'
+    img: '//kongfandong.cn/images/capture/Verse.png'
   },
   4: {
     label: 'Search',
     text: '搜索栏',
-    img: 'http://cdn.kongfandong.cn/img/Search.png'
+    img: '//kongfandong.cn/images/capture/Search.png'
   },
   5: {
     label: 'Collection',
     text: '导航收藏页',
-    img: 'http://cdn.kongfandong.cn/img/Collection.png'
+    img: '//kongfandong.cn/images/capture/Collection.png'
   },
   6: {
     label: 'Iframe',
     text: '外部网站',
-    img: 'http://cdn.kongfandong.cn/img/Iframe.png'
+    img: '//kongfandong.cn/images/capture/Iframe.png'
   },
   7: {
     label: 'TodoList',
     text: '备忘清单',
-    img: 'http://cdn.kongfandong.cn/img/TodoList.png'
+    img: '//kongfandong.cn/images/capture/TodoList.png'
   },
   8: {
     label: 'Weather',
     text: '天气',
-    img: 'http://cdn.kongfandong.cn/img/Weather.png'
+    img: '//kongfandong.cn/images/capture/Weather.png'
   },
   9: {
     label: 'CountDown',
     text: '倒计时',
-    img: 'http://cdn.kongfandong.cn/img/CountDown.png'
+    img: '//kongfandong.cn/images/capture/CountDown.png'
   },
   10: {
     label: 'JuejinList',
     text: '掘金',
-    img: 'http://cdn.kongfandong.cn/img/JuejinList.png'
+    img: '//kongfandong.cn/images/capture/JuejinList.png'
   },
   11: {
     label: 'WeiboList',
     text: '微博热搜',
-    img: 'http://cdn.kongfandong.cn/img/WeiboList.png'
+    img: '//kongfandong.cn/images/capture/WeiboList.png'
   },
   12: {
     label: 'GithubTrending',
     text: 'Github趋势',
-    img: 'http://cdn.kongfandong.cn/img/GithubTrending.png'
+    img: '//kongfandong.cn/images/capture/GithubTrending.png'
   }
 }
 
