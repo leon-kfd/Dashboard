ea0e55730a204c13cc76df642c49d457e49ce755
leon-kfd <kfd_personal@163.com>
2021-10-10 10:33:55 +0800

    feat: add a video link to demo

--- src/components/Axuiliary/About.vue
+++ src/components/Axuiliary/About.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="wrapper">
-    <p class="title">项目说明</p>
-    <ul class="list">
-      <li>项目基于<code>MIT</code>协议开源，源码地址访问此处<a href="https://github.com/leon-kfd/Dashboard" target="_blank">Github</a></li>
-      <li>项目基于<code>Vite</code>、<code>Vue3</code>、<code>Typescript</code>，欢迎各种PR贡献你的想法</li>
-      <li>若对此项目有想法或建议可在<a href="https://github.com/leon-kfd/Dashboard" target="_blank">Github</a>上提<code>issue</code>或者留言于<a href="mailto:kfd_personal@163.com">kfd_personal@163.com</a></li>
-    </ul>
+    <div class="item">
+      <p class="title">项目说明</p>
+      <ul class="list">
+        <li>项目基于<code>MIT</code>协议开源，源码地址访问此处<a href="https://github.com/leon-kfd/Dashboard" target="_blank">Github</a></li>
+        <li>项目基于<code>Vite</code>、<code>Vue3</code>、<code>Typescript</code>，欢迎各种PR贡献你的想法</li>
+        <li>若对此项目有想法或建议可在<a href="https://github.com/leon-kfd/Dashboard" target="_blank">Github</a>上提<code>issue</code>或者留言于<a href="mailto:kfd_personal@163.com">kfd_personal@163.com</a></li>
+      </ul>
+    </div>
+    <div class="item">
+      <p class="title">项目演示视频</p>
+      <a class="video-wrapper" href="https://www.bilibili.com/video/BV1Vu411Z7i1?share_source=copy_web" target="_blank">
+        <img src="imgs/video-thumb.png" alt="">
+        <div class="mask">
+          <i class="el-icon-video-play"></i>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -18,9 +29,13 @@ export default defineComponent({
 <style lang="scss" scoped>
 .wrapper {
   padding: 10px;
+  .item {
+    margin-bottom: 20px;
+  }
   .title {
     color: $--color-grey1;
     margin-bottom: 10px;
+    font-weight: bold;
   }
   .list {
     padding-left: 25px;
@@ -56,6 +71,30 @@ export default defineComponent({
           text-decoration: underline;
         }
       }
+    }
+  }
+  .video-wrapper {
+    display: block;
+    width: 100%;
+    position: relative;
+    img {
+      width: 100%;
+      height: auto;
+    }
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 48px;
+      color: #fff;
+      cursor: pointer;
+      background: rgba(0,0,0,.4);
+      backdrop-filter: blur(3px);
     }
   }
 }
