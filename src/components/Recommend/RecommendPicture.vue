104195f014e0e8104019cd554d22d4a886382b77
leon-kfd <kfd_personal@163.com>
2021-12-06 22:45:47 +0800

    feat: recommend picture add 360 img

--- src/components/Recommend/RecommendPicture.vue
+++ src/components/Recommend/RecommendPicture.vue
@@ -3,22 +3,24 @@
   <animation-dialog
     ref="dialog"
     animationMode
-    customWrapperClass="backdrop-blur"
+    customWrapperClass="backdrop-blur recommend-picture"
     title="今日壁纸推荐"
     width="min(760px, 94vw)"
-    height="min(480px, 80vh)"
+    height="min(520px, 80vh)"
     appendToBody
     :closeOnClickOutside="false"
     :listenWindowSizeChange="true"
     @beforeClose="close">
     <div class="wrapper" v-if="beginLoad">
-      <div class="box">
-        <div class="title-wrapper clear">
-          <span class="title fl">必应壁纸</span>
-        </div>
-        <div class="text" v-if="bingLoading">Loading...</div>
-        <div class="text" v-else-if="bingError">Something Error...</div>
-        <div class="bing item-wrapper" v-else>
+      <div class="tab-title-wrapper">
+        <div :class="['title', tabIndex === 1 && 'active']" @click="tabIndex = 1">必应壁纸</div>
+        <div :class="['title', tabIndex === 3 && 'active']" @click="tabIndex = 3">360壁纸</div>
+        <div :class="['title', tabIndex === 2 && 'active']" @click="tabIndex = 2">UNSPLAH</div>
+      </div>
+      <div class="tab-container" v-show="tabIndex === 1">
+        <div class="text" v-if="loading">Loading...</div>
+        <div class="text" v-else-if="error">Something Error...</div>
+        <div class="item-wrapper" v-else>
           <div class="item" v-for="item in bingList" :key="item.url" @click="handleSelect(item.url)">
             <div class="img-wrapper">
               <img v-if="item.url" :src="item.thumb" loading="lazy" />
@@ -27,20 +29,38 @@
           <div class="item-fake" v-for="item in 4" :key="item"></div>
         </div>
       </div>
-      <div class="box">
-        <div class="title-wrapper clear">
-          <span class="title fl">Unsplash</span>
-          <span class="date fr" v-if="unsplashDate">{{unsplashDate}}</span>
-        </div>
-        <div class="text" v-if="unsplashLoading">Loading...</div>
-        <div class="text" v-else-if="unsplashError">Something Error...</div>
-        <div class="unsplash item-wrapper" v-else>
-          <div class="item" v-for="item in unsplashList" :key="item.id" @click="handleSelect(item.urls.raw)">
+      <div class="tab-container" v-show="tabIndex === 2">
+        <div class="text" v-if="loading">Loading...</div>
+        <div class="text" v-else-if="error">Something Error...</div>
+        <div class="item-wrapper" v-else>
+          <div class="item" v-for="item in unsplashList" :key="item.url" @click="handleSelect(item.url)">
             <div class="img-wrapper">
-              <img v-if="item.urls.thumb" :src="item.urls.thumb" loading="lazy" />
+              <img v-if="item.url" :src="item.thumb" loading="lazy" />
             </div>
           </div>
           <div class="item-fake" v-for="item in 4" :key="item"></div>
+        </div>
+      </div>
+      <div class="sub-tab-container" v-show="tabIndex === 3">
+        <div
+          :class="['sub-tab-title', p360CatalogActive === item.value && 'active']"
+          v-for="item in p360Catalog"
+          :key="item.value"
+          @click="p360CatalogActive = item.value"
+        >{{item.label}}
+        </div>
+      </div>
+      <div class="tab-container" v-show="tabIndex === 3">
+        <div class="item-wrapper">
+          <div class="item" v-for="item in p360List" :key="item.url" @click="handleSelect(item.url)">
+            <div class="img-wrapper">
+              <img v-if="item.url" :src="item.thumb" loading="lazy" />
+            </div>
+          </div>
+          <div class="item-fake" v-for="item in 4" :key="item"></div>
+        </div>
+        <div class="load-more">
+          <button class="btn btn-warning" :loading="loading" @click="loadP360More">{{ loading ? 'LOADING...': 'LOAD MORE' }}</button>
         </div>
       </div>
     </div>
@@ -48,19 +68,40 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { apiURL } from '@/global'
+type ListItem = {
+  thumb: string;
+  url: string
+}
 const emit = defineEmits(['submit'])
 const beginLoad = ref(false)
 const dialog = ref()
 
-const bingList = ref<{ url: string, thumb: string }[]>([])
-const bingLoading = ref(false)
-const bingError = ref(false)
+const tabIndex = ref(1)
+const loading = ref(false)
+const error = ref(false)
+const bingList = ref<ListItem[]>([])
+const unsplashList = ref<ListItem[]>([])
+const p360List = ref<ListItem[]>([])
+const page = ref(1)
+
+const p360Catalog = [
+  { label: '风景', value: 9 },
+  { label: '4K', value: 36 },
+  { label: '清新', value: 15 },
+  { label: '萌宠', value: 14 },
+  { label: '游戏', value: 5 },
+  { label: '汽车', value: 12 },
+  { label: '时尚', value: 10 },
+  { label: '体育', value: 16 }
+]
+const p360CatalogActive = ref(9)
+
 const getBingList = async () => {
   try {
-    bingLoading.value = true
-    bingError.value = false
+    loading.value = true
+    error.value = false
     const bingTarget = encodeURIComponent('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=zh-CN')
     const res = await fetch(`${apiURL}/api/transfer?target=${bingTarget}&noHeaders=1`, {
       headers: {
@@ -81,43 +122,88 @@ const getBingList = async () => {
       throw new Error('Api server error')
     }
   } catch (e) {
-    bingError.value = true
+    error.value = true
     console.error(e)
   } finally {
-    bingLoading.value = false
+    loading.value = false
   }
 }
 
-const unsplashList = ref<any[]>([])
-const unsplashDate = ref('')
-const unsplashLoading = ref(false)
-const unsplashError = ref(false)
 const getUnsplashList = async () => {
   try {
-    unsplashLoading.value = true
-    unsplashError.value = false
-    const res = await fetch(`${apiURL}/photos`)
+    loading.value = true
+    error.value = false
+    const res = await fetch(`${apiURL}/photos?pageSize=18`)
     const { errCode, data } = await res.json()
     if (errCode === 200) {
-      unsplashList.value = data.list
-      unsplashDate.value = data.date
+      unsplashList.value = data.list.map((item: any) => {
+        return {
+          url: item.urls.raw,
+          thumb: item.urls.thumb
+        }
+      })
     } else {
       throw new Error('Api server error')
     }
   } catch (e) {
-    unsplashError.value = true
+    error.value = true
     console.error(e)
   } finally {
-    unsplashLoading.value = false
+    loading.value = false
+  }
+}
+
+const getP360List = async (resetPage = false) => {
+  if (resetPage) {
+    p360List.value = []
+    page.value = 1
+  }
+  try {
+    loading.value = true
+    error.value = false
+    const res = await fetch(`${apiURL}/api/360img?cid=${p360CatalogActive.value}&page=${page.value}&pageSize=18`)
+    const { errno, data } = await res.json()
+    if (errno === 0) {
+      p360List.value = [
+        ...p360List.value,
+        ...data.list.map((item: any) => {
+          return {
+            url: item.url,
+            thumb: `${item.url}?x-oss-process=image/resize,w_300/quality,q_90`
+          }
+        })
+      ]
+    } else {
+      throw new Error('Api server error')
+    }
+  } catch (e) {
+    error.value = true
+    console.error(e)
+  } finally {
+    loading.value = false
   }
 }
 
 const handleOpenSelector = () => {
   dialog.value.open()
   if (!beginLoad.value) beginLoad.value = true
-  if (unsplashError.value || unsplashList.value.length === 0) getUnsplashList()
-  if (bingError.value || bingList.value.length === 0) getBingList()
 }
+
+watch(() => [beginLoad.value, tabIndex.value, p360CatalogActive.value], () => {
+  if (tabIndex.value === 1) {
+    if (!bingList.value || bingList.value.length === 0) getBingList()
+  } else if (tabIndex.value === 2) {
+    if (!unsplashList.value || unsplashList.value.length === 0) getUnsplashList()
+  } else if (tabIndex.value === 3) {
+    getP360List(true)
+  }
+})
+
+const loadP360More = () => {
+  page.value++
+  getP360List()
+}
+
 const close = () => {}
 const handleSelect = (url: string) => {
   emit('submit', url)
@@ -127,40 +213,82 @@ const handleSelect = (url: string) => {
 </script>
 <style lang="scss" scoped>
 .wrapper {
-  .box {
-    margin-bottom: 16px;
-    .text {
-      font-size: 16px;
-      color: #888;
-      line-height: 28px;
+  height: 100%;
+  flex-direction: column;
+  display: flex;
+  user-select: none;
+  .text {
+    font-size: 16px;
+    color: #888;
+    line-height: 28px;
+  }
+  .tab-title-wrapper {
+    display: flex;
+    .title {
+      margin-right: 20px;
+      font-size: 18px;
+      color: rgb(43, 43, 43);
+      font-weight: bold;
+      line-height: 22px;
+      font-size: 18px;
+      position: relative;
+      margin-bottom: 5px;
+      cursor: pointer;
+      &:after {
+        position: absolute;
+        content: '';
+        left: 0;
+        width: 100%;
+        bottom: 0;
+        height: 8px;
+        background: rgba($--color-warning, .2);
+        transform: scale(0);
+        transition: transform .3s ease-in-out;
+      }
+      &.active:after {
+        transform: scale(1);
+      }
     }
   }
-}
-.title-wrapper {
-  .title {
-    color: rgb(43, 43, 43);
-    font-weight: bold;
-    line-height: 22px;
-    font-size: 18px;
-    position: relative;
-    margin-bottom: 5px;
-    &:after {
-      position: absolute;
-      content: '';
-      left: 0;
-      width: 100%;
-      bottom: 0;
-      height: 8px;
-      background: rgba($--color-warning, .2);
+  .sub-tab-container {
+    display: flex;
+    align-items: baseline;
+    flex-wrap: wrap;
+    margin: 8px 0;
+    .sub-tab-title {
+      font-size: 14px;
+      position: relative;
+      cursor: pointer;
+      &:hover,
+      &.active {
+        color: $--color-primary;
+      }
+      &:not(:last-child){
+        margin-right: 6px;
+        padding-right: 6px;
+        &:after {
+          content: '';
+          position: absolute;
+          right: 0;
+          top: 2px;
+          height: calc(100% - 4px);
+          border-right: 1px solid #eee;
+        }
+      }
     }
   }
-  .date {
-    font-size: 14px;
-    color: #999;
-    line-height: 22px;
-    font-weight: bold;
+  .tab-container {
+    height: 100%;
+    flex: 1;
+    overflow-y: auto;
+  }
+
+  .load-more {
+    text-align: center;
+    margin: 18px 0;
   }
 }
+
 .item-wrapper {
   display: flex;
   flex-wrap: wrap;
@@ -208,6 +336,13 @@ const handleSelect = (url: string) => {
 @media screen and (max-width: 480px) {
   .item-wrapper {
     font-size: 12px;
+  }
+}
+</style>
+<style lang="scss">
+.recommend-picture {
+  .dialog .dialog-body {
+    margin-top: -10px;
   }
 }
 </style>
