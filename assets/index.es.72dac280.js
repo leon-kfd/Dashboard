995ba36e9d0d32a640649fb17869be30af32d528
leon-kfd <kfd_personal@163.com>
2022-01-10 14:03:18 +0000

    Deploying to gh-pages from  @ 5976b2ce17376e88ff705826430e6211202abbf6 🚀

--- assets/index.es.72dac280.js
+++
@@ -1,1 +0,0 @@
-import{a as s,b as o,e as a,V as e,X as i,k as r,Y as d}from"./index.ca1a94fe.js";import"./index.12b96058.js";import"./color.556e4bcb.js";const m=s(),t=s(),c=o((()=>({commands:()=>[a(m,(()=>d)),a(t,(()=>e))],prosePlugins:()=>[i(),r({"Mod-z":d,"Mod-y":e,"Shift-Mod-z":e})]})))();export{t as Redo,m as Undo,c as history};
