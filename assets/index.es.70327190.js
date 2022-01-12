df934087c72a0c88cf728152086b2101a1af7ca4
leon-kfd <kfd_personal@163.com>
2022-01-12 14:38:28 +0000

    Deploying to gh-pages from  @ a3bfb55569b6af13531cb84e1d17db76497e02fb 🚀

--- assets/index.es.70327190.js
+++
@@ -1,1 +0,0 @@
-import{a as s,b as o,e as a,V as i,X as r,k as d,Y as e}from"./index.b4818616.js";import"./index.0844a28a.js";import"./color.556e4bcb.js";const m=s(),t=s(),n=o((()=>({commands:()=>[a(m,(()=>e)),a(t,(()=>i))],prosePlugins:()=>[r(),d({"Mod-z":e,"Mod-y":i,"Shift-Mod-z":i})]})))();export{t as Redo,m as Undo,n as history};
---
+++ assets/index.es.8ca30757.js
@@ -0,0 +1,1 @@
+import{a as s,b as o,e as a,V as e,X as i,k as r,Y as d}from"./index.a401c4a9.js";import"./index.fe526e92.js";import"./color.556e4bcb.js";const m=s(),t=s(),c=o((()=>({commands:()=>[a(m,(()=>d)),a(t,(()=>e))],prosePlugins:()=>[i(),r({"Mod-z":d,"Mod-y":e,"Shift-Mod-z":e})]})))();export{t as Redo,m as Undo,c as history};
