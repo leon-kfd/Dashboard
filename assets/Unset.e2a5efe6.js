181db34d9af7128959199db9b5ff02983307f67d
leon-kfd <kfd_personal@163.com>
2022-04-02 14:29:15 +0000

    Deploying to gh-pages from  @ 5eae3c3de5f0bfd714323ec59158b9c75aeec8fa 🚀

--- assets/Unset.e2a5efe6.js
+++
@@ -1,1 +0,0 @@
-import{d as t,_ as s,o as n,k as a,t as o}from"./index.2d2dbb39.js";const r=t({name:"Unset",props:{tips:{type:String}}}),_={class:"unset-wrapper"};function p(e,i,c,d,u,f){return n(),a("div",_,o(e.tips||"\u{1F528}Wait for setting"),1)}var m=s(r,[["render",p],["__scopeId","data-v-ad495116"]]);export{m as U};
--- assets/index.es.c33c196d.js
+++
@@ -1,1 +0,0 @@
-import{a as t,b as e,g as s,U as o,V as r,k as d,X as a}from"./index.934c7ee4.js";import"./index.2d2dbb39.js";import"./color.556e4bcb.js";const m=t(),i=t(),y=e(()=>({commands:()=>[s(m,()=>a),s(i,()=>o)],prosePlugins:()=>[r(),d({"Mod-z":a,"Mod-y":o,"Shift-Mod-z":o})]}))();export{i as Redo,m as Undo,y as history};
