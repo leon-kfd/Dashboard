5dc8be242d2bab4ec8145c73e56d74a8f68b5a50
kfd <kfd_personal@163.com>
2023-08-25 11:09:11 +0800

    refactor: remove electron build config

--- scripts/dist-electron.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-try {
-  fs.renameSync('install/howdy-dashboard.exe', 'dist/howdy-dashboard.exe')
-  fs.renameSync('install/howdy-dashboard.zip', 'dist/howdy-dashboard.zip')
-  console.log('move electorn package success!')
-} catch (e) {
-  console.error(e)
-}
